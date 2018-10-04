25f3503b86541e98b3bf99528c96f43393f94ddd
resad384 <resad384@gmail.com>
2018-10-04 17:04:07 +0400

    Add project files.

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace App1
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        private List<Quote> _quotes;
+        public MainPage()
+        {
+            InitializeComponent();
+            _quotes = Quote.GetQuotes();
+            quotetext.Text = _quotes.First().Text;
+           
+        }
+
+        int index = 0;
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+           if (index == _quotes.Count - 1) { index = -1; }
+           index++;
+           quotetext.Text = _quotes[index].Text;
+        }
+    }
+}
---
+++ App1/App1/Quotes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Quote
+    { 
+        public string Author { get; set; }
+        public string Text { get; set; }
+
+        static public  List<Quote> GetQuotes()
+        {
+            var quotes = new List<Quote>();
+            quotes.Add(new Quote{ Author = "Insanlar", Text = "Salam Ne varx ne yox" } );
+            quotes.Add(new Quote { Author = "Heyvanlar", Text = "Myau MYau" });
+            quotes.Add(new Quote { Author = "Robotlar", Text = "020651650230320203" });
+
+            return quotes;
+        }
+    }
+}
