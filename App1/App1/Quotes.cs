3d1c4adea7c686d600c05d166cd61e1306c828d3
resad384 <resad384@gmail.com>
2018-10-05 11:54:50 +0400

    zzz

--- App1/App1/Quotes.cs
+++ App1/App1/Quotes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace App1
 {     
-    //salam  commit1jjjj hiiuuiibui
+    //salam  commit1jjjj hiiuuiibui  sveüeüüeevev
     class Quote
     {   
         public string Author { get; set; }
